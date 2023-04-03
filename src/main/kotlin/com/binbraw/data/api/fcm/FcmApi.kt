73d59cdd71e7441fe6081814d615ee7302a18501
fahmirumagutawan <fahmigutawan@gmail.com>
2023-04-03 18:48:11 +0700

    Fixed add SOS notification endpoint

--- src/main/kotlin/com/binbraw/data/api/fcm/FcmApi.kt
+++ src/main/kotlin/com/binbraw/data/api/fcm/FcmApi.kt
@@ -1,5 +1,6 @@
 package com.binbraw.data.api.fcm
 
+import com.binbraw.data.api.notification.NotificationApi
 import com.binbraw.data.table.family.PatientWithFamilyTable
 import com.binbraw.data.table.fcm.FcmTokenTable
 import com.binbraw.data.table.user.UserTable
@@ -17,6 +18,7 @@ import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 import io.ktor.server.request.*
 import io.ktor.server.routing.*
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.update
@@ -89,11 +91,11 @@ object FcmApi : KoinComponent {
                 "Unknown"
             }
 
-            familyIds.forEach { family_id ->
+            familyIds.forEach { id ->
                 val token = try {
                     transaction {
                         fcmTokenTable.select {
-                            fcmTokenTable.uid eq UUID.fromString(family_id)
+                            fcmTokenTable.uid eq UUID.fromString(id)
                         }.mapNotNull {
                             it[fcmTokenTable.token]
                         }
@@ -119,12 +121,29 @@ object FcmApi : KoinComponent {
                         )
                     )
                     contentType(ContentType.Application.Json)
+                }.let{
+                    if(it.status == HttpStatusCode.OK){
+                        transaction {
+                                val randomizedNotifId = UUID.randomUUID()
+                                NotificationApi.notifTable.insert {
+                                    it[NotificationApi.notifTable.notification_id] = randomizedNotifId
+                                    it[NotificationApi.notifTable.title] = "SOS Notification from $patientName"
+                                    it[NotificationApi.notifTable.body] = "Something happens to $patientName, please check or call"
+                                    it[NotificationApi.notifTable.additional_link] = "https://www.google.com/maps/search/${receivedBody.latitude},${receivedBody.longitude}"
+                                    it[NotificationApi.notifTable.timestamp] = "DUMMY"
+                                    it[NotificationApi.notifTable.uid] = uid
+                                    it[NotificationApi.notifTable.lat] = receivedBody.latitude
+                                    it[NotificationApi.notifTable.long] = receivedBody.longitude
+                                    it[NotificationApi.notifTable.notif_type_id] = 3
+                                }
+                        }
+                    }
                 }
             }
 
             sendGeneralResponse<Any>(
                 success = true,
-                message = "Send notification succeeded",
+                message = "Add SOS notification success",
                 code = HttpStatusCode.OK
             )
         }
